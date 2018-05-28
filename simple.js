6aed0f033af4d724602d5f895a82621d5b1c6eb7
alexzandra <a.wascikowska@gmail.com>
2018-05-28 14:38:05 +0200

    search for page ver clean

---
+++ simple.js
@@ -0,0 +1,21 @@
+const chromedriver = require("chromedriver");
+const selenium = require("selenium-webdriver")
+const driver = new selenium.Builder().forBrowser("chrome").build();
+driver.get("http://google.com/");
+
+const By = selenium.By;
+const until = selenium.until;
+
+const locators = {
+    searchForm: By.css("form"),
+    searchBox: By.css("#sb_ifc0 input[name='q']"),
+    KRS: By.partialLinkText('KRS')
+};
+
+function findBushtree(q) {
+    driver.findElement(locators.searchBox)
+        .sendKeys(q);
+    driver.findElement(locators.searchForm).submit();
+};
+
+findBushtree("bushtree");

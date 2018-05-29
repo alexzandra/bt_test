93161b55cb6508bf2c0459e5a273466086e1233d
alexzandra <a.wascikowska@gmail.com>
2018-05-29 13:29:33 +0200

    nip and take screenshot

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const chromedriver = require("chromedriver");
 const selenium = require("selenium-webdriver");
+const fs = require("fs");
 const driver = new selenium.Builder().forBrowser("chrome").build();
-
 
 const By = selenium.By;
 const until = selenium.until;
@@ -9,7 +9,9 @@ const until = selenium.until;
 const locators = {
     searchForm: By.css('form'),
     searchBox: By.css('#lst-ib'),
-    KRS: By.partialLinkText('krs')
+    KRS: By.partialLinkText('krs'),
+    NIP: By.partialLinkText('5542961359'),
+    bushtreeTable: By.css('#main > div:nth-child(4)')
 };
 
 function findBushtree(q) {
@@ -17,14 +19,25 @@ function findBushtree(q) {
         .sendKeys(q).then( () => driver.findElement(locators.searchForm).submit() );
 };
 
-
 function openKRS() {
-   return  driver.wait(until.elementLocated(locators.KRS), 20000).then( ()=>
+   return  driver.wait(until.elementLocated(locators.KRS), 10000).then( () =>
     driver.findElement(locators.KRS).click());
 };
 
+function KRSscreenshot() {
+    return driver.wait(until.elementLocated(locators.NIP), 10000).then( () =>
+    driver.takeScreenshot().then((image, err) => {
+        fs.writeFile("KRS.jpg", image, "base64",
+            err => console.error(err));
+        })
+)};
+
 function KRS() {
-    driver.get("http://google.com/").then( ()=> findBushtree("bushtree")).then(() => openKRS());
+    driver.get("http://google.com/").then( () => 
+        findBushtree("bushtree")).then( () => 
+        openKRS().then( () =>
+        KRSscreenshot()
+    ))
 };
 
 KRS();

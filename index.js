313015d98a66fd192aef2c311f7fb6b3a5f204ed
alexzandra <a.wascikowska@gmail.com>
2018-05-30 12:08:12 +0200

    sprawdza KRS, NIP + drukuje NIP z tabeli

--- index.js
+++ index.js
@@ -9,35 +9,42 @@ const until = selenium.until;
 const locators = {
     searchForm: By.css('form'),
     searchBox: By.css('#lst-ib'),
-    KRS: By.partialLinkText('krs'),
+    KRS: By.partialLinkText('KRS'),
+    bushtreeKRS: By.partialLinkText('0000721490'),
     NIP: By.partialLinkText('5542961359'),
-    bushtreeTable: By.css('#main > div:nth-child(4)')
+    NIPtable: By.xpath('//*[@id="main"]/div[4]/table[1]/tbody/tr[3]/td[2]')
 };
 
 function findBushtree(q) {
-     return driver.findElement(locators.searchBox)
+    return driver.findElement(locators.searchBox)
         .sendKeys(q).then( () => driver.findElement(locators.searchForm).submit() );
 };
 
 function openKRS() {
    return  driver.wait(until.elementLocated(locators.KRS), 10000).then( () =>
-    driver.findElement(locators.KRS).click());
+        driver.findElement(locators.KRS).click());
 };
 
 function KRSscreenshot() {
-    return driver.wait(until.elementLocated(locators.NIP), 10000).then( () =>
+    return driver.wait(until.elementLocated(locators.bushtreeKRS), 10000).then( () =>
     driver.takeScreenshot().then((image, err) => {
         fs.writeFile("KRS.jpg", image, "base64",
             err => console.error(err));
-        })
+        }))
+};
+
+function printNIP() {
+    return driver.wait(until.elementLocated(locators.NIP), 10000).then( () =>
+    driver.findElement(locators.NIPtable).getText().then(txt => console.log("BUSHTREE NIP:", txt))
 )};
 
 function KRS() {
     driver.get("http://google.com/").then( () => 
         findBushtree("bushtree")).then( () => 
         openKRS().then( () =>
-        KRSscreenshot()
-    ))
+        KRSscreenshot().then( () =>
+        printNIP()
+         )))
 };
 
 KRS();

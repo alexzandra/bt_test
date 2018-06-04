571ae0e225ccab61534deaf178a33dcf5a60473b
alexzandra <a.wascikowska@gmail.com>
2018-06-04 13:18:14 +0200

    asercjaKRS + walidacjaNIP +errors v2
    (Cannot read property 'strictEqual' of undefined
    validateNIP(...).then is not a function)

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const locators = {
     bushtreeKRS: By.partialLinkText('0000721490'),
     NIP: By.partialLinkText('5542961359'),
     KRStable: By.xpath('//*[@id="main"]/div[4]/table[1]/tbody/tr[4]/td[2]'),
-    NIPtable: By.xpath('//*[@id="main"]/div[4]/table[1]/tbody/tr[3]/td[2]')
+    NIPtable: By.xpath('//*[@id="main"]/div[4]/table[1]/tbody/tr[4]/td[2]')
 };
 
 function findBushtree(q) {
@@ -47,35 +47,35 @@ function printNIP() {
 )};
 
 
-const NIP = driver.findElement(locators.NIPtable).getText();
-function validateNIP(NIP) {
+function validateNIP() {
+    var NIP = driver.findElement(locators.NIPtable).getText();
+   
     if (NIP == null)
         return false;
   
     if (NIP.length != 10)
         return false;
-    
-    var reg = /^[0-9]{10}$/;
-    if(reg.test(NIP) == false) {
-        return false}
-    else
-    {
-        var digits = (""+NIP).split("");
-        var checksum = (6*parseInt(digits[0]) +
-                        5*parseInt(digits[1]) + 
-                        7*parseInt(digits[2]) + 
-                        2*parseInt(digits[3]) + 
-                        3*parseInt(digits[4]) + 
-                        4*parseInt(digits[5]) + 
-                        5*parseInt(digits[6]) + 
-                        6*parseInt(digits[7]) + 
-                        7*parseInt(digits[8])
-                        )%11;
+          
+    for (i=0; i<10; i++)
+        if (isNaN(nip[i]))
+            return false;
+
+      sum = 6 * nip[0] +
+            5 * nip[1] +
+            7 * nip[2] +
+            2 * nip[3] +
+            3 * nip[4] +
+            4 * nip[5] +
+            5 * nip[6] +
+            6 * nip[7] +
+            7 * nip[8];
+            sum %= 11;
          
-        return (parseInt(digits[9])==checksum);
-                    
-    }
-        };
+            if (nip[9] != sum)
+            return false;
+             
+            return true;
+    };
 
 function KRS() {
     driver.get("http://google.com/").then( () => 
@@ -83,11 +83,11 @@ function KRS() {
         findBushtree("bushtree")).then( () => 
         openKRS().then( () =>
         KRSscreenshot().then( () =>
-    //    KRSassert().then( () =>
+      //  KRSassert().then( () =>
         validateNIP().then( () =>    
         printNIP()
          )))))
-        //)
+       // )
 };
 
 KRS();
